2cafc866429547a20d7fabf745c5ff8c3c91e500
Mustafa Nawzat <mustafanawzatt@gmail.com>
2020-02-29 23:32:41 +0300

    styling the pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import './App.css';
 import movies from './data';
 import Movieslist from './MoviesList';
+// 
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+
 export default class App extends React.Component {
   render() {
     console.log(movies.results);
@@ -9,8 +18,25 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <Movieslist list={movies.results}/>
+        <Container>
+          <Navbar expand="lg">
+            <Navbar.Brand className="text-light logo" href="javascript:void(0)">To Watch Next</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Form inline className="right">
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-light">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+          </Container>
         </header>
+
+        <Container>
+          <Row>
+              <Movieslist list={movies.results}/>
+          </Row>  
+        </Container>
       </div>
     );
   }
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
 
 export default class MoviesList extends React.Component {
   render() {
@@ -6,14 +9,27 @@ export default class MoviesList extends React.Component {
     
       const BACKDROP_BASE_URL = 'http://image.tmdb.org/t/p/w780'
       return (
-       <div>
+        <>
+           
           {this.props.list.map(el =>
-          <div>
-            <h2>{el['original_title']}</h2>
-            <img src={BACKDROP_BASE_URL + el.poster_path}/>
-           </div>
-          )}  
-       </div>
+          <>
+          <Col md="4">
+            <Card className="bg-light">
+              <Card.Img className="movieImg" src={BACKDROP_BASE_URL + el.poster_path}/>
+              <h3>{el['original_title']}</h3>
+              <Card.Text>
+                <ul className="list-unstyled infoList">
+                  <li><i className="fa fa-star" aria-hidden="true"></i> {el['vote_average']}</li>
+                  <li>Release date:<br></br>{el['release_date']}</li>
+                  <li><p className="overviewLi">{el['overview']}</p> <a href="javascript:void(0)">read more</a></li>
+                  
+                </ul>
+              </Card.Text>
+            </Card>
+          </Col>
+          </>
+          )} 
+        </> 
       )
     }
 }
