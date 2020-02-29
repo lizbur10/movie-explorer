b25df483933ac0797acee040e7026b80b4b7f102
naderalfakesh <nader.alfakesh@gmail.com>
2020-02-29 16:43:06 +0300

    styling the movies list and header

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React  from 'react'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles({
+    header: {
+      width:"100%"
+    },
+  });
 
-export default class Header extends Component {
-    render() {
-        return (
-            <div>
-                This is header
+
+export default function Header() {
+    const classes = useStyles();
+    return (
+        <div className={classes.header}>
+                <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="baseline"
+            >
+
+                <h1>Movie-Ex</h1>
+                <form  noValidate autoComplete="off">
+                <TextField  id="standard-search" label="Movies" type="search" />
+                <Button variant="contained" color="primary" size="large">
+                Search
+                </Button>
+                </form>
+            </Grid>
             </div>
-        )
-    }
+    )
 }
+
+
--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import MovieCard from './MovieCard';
+
 
 export default class MoviesList extends Component {
     render() {
         const baseURL= "http://image.tmdb.org/t/p/w1280/"
-        // console.log(this.props.list) 
-        const list = this.props.list.map(movie => 
-            <div key={movie.id}>
-                <h1>{movie.original_title}</h1>
-                <img src={baseURL + movie.poster_path} width="600" alt="Poster" />
-            </div>
-    )      
+         
+    // console.log(movie)
+
         return <div>
+            <Grid
+                container
+                direction="row"
+                justify="center"
+                alignItems="stretch"
+    
+                >
             {
                 this.props.list.map(movie => 
-                    <div key={movie.id}>
-                        <h1>{movie.original_title}</h1>
-                        <img src={baseURL + movie.poster_path} width="600" alt="Poster" />
-                    </div>
+                    <Grid item xs={4} key={movie.id} style={{marginBottom: "50px"}} >
+                    <MovieCard title={movie.original_title}
+                     link={baseURL + movie.poster_path}
+                     date={movie.release_date}
+                     overview={movie.overview}
+                     
+                     />
+                  </Grid>
             )      
             }
+            </Grid>
         </div>
     }
 }
